2d241f77157ef3895168175c61603014c2887b14
oscar1223 <oscararauzp@gmail.com>
2025-01-03 19:49:56 +0100

    geometries

--- 08-geometries/src/script.js
+++ 08-geometries/src/script.js
@@ -1,5 +1,4 @@
 import * as THREE from 'three'
-import './style.css'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 /**
@@ -8,83 +7,66 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
+const geometry = new THREE.BufferGeometry()
+
+const count = 100
+const positionArray = new Float32Array(count * 3 * 3)
+
+for(let i = 0; i < count * 3 * 3; i++)
+{
+    positionArray[i] = Math.random()
+}
+
+const positionAtribute = new THREE.BufferAttribute(positionArray, 3)
+geometry.setAttribute('position', positionAtribute)
+
 // Scene
 const scene = new THREE.Scene()
 
-/**
- * Object
- */
-const geometry = new THREE.BoxGeometry(1, 3, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// Object
+//const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
-/**
- * Sizes
- */
+// Sizes
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
 }
 
-// Resizing components
-window.addEventListener('resize', () => 
+window.addEventListener('resize', () =>
 {
-    console.log('window has been resized')
     // Update sizes
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
 
-    // Update cameras
+    // Update camera
     camera.aspect = sizes.width / sizes.height
     camera.updateProjectionMatrix()
 
-    //Update rederer
+    // Update renderer
     renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
-const fullscreenElement = document.fullscreenElement || document.webkitFullscreenElement
-
-window.addEventListener('dblclick', () => {
-    if(!document.fullscreenElement)
-    {
-        if(canvas.requestFullscreen){
-            canvas.requestFullscreen()
-        }else if(canvas.webkitFullscreenElement){
-            canvas.webkitrequestFullscreen()
-        }
-
-    }else
-    {
-        console.log('leave full screen')
-    }
-})
-
-/**
- * Camera
- */
-// Base camera
+// Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.z = 3
 scene.add(camera)
 
 // Controls
 const controls = new OrbitControls(camera, canvas)
-controls.enabled = true
 controls.enableDamping = true
 
-/**
- * Renderer
- */
+// Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-/**
- * Animate
- */
+// Animate
 const clock = new THREE.Clock()
 
 const tick = () =>
