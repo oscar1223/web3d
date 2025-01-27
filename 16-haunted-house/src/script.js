96b281d47506089505d1b8b4b319a19cffd54d01
oscar1223 <oscararauzp@gmail.com>
2025-01-27 16:20:52 +0100

    working in to hounted house

--- 16-haunted-house/src/script.js
+++ 16-haunted-house/src/script.js
@@ -18,19 +18,100 @@ const scene = new THREE.Scene()
 /**
  * House
  */
-// Temporary sphere
-const sphere = new THREE.Mesh(
-    new THREE.SphereGeometry(1, 32, 32),
-    new THREE.MeshStandardMaterial({ roughness: 0.7 })
-)
-scene.add(sphere)
+
 
 // Floor
 const floor = new THREE.Mesh(
     new THREE.PlaneGeometry(20, 20),
     new THREE.MeshStandardMaterial({ roughness: 0.7 })
 )
+floor.rotation.x = - Math.PI * 0.5
 scene.add(floor)
+
+// Creating group for the house
+const house = new THREE.Group()
+scene.add(house)
+
+// walls
+const walls = new THREE.Mesh(
+    new THREE.BoxGeometry(4, 2.5, 4),
+    new THREE.MeshStandardMaterial({ roughness: 0.7 })
+)
+walls.position.y = 1.25
+
+house.add(walls)
+
+// Roof
+const roof = new THREE.Mesh(
+    new THREE.ConeGeometry(3.5, 1, 4),
+    new THREE.MeshStandardMaterial({ roughness: 0.7 })
+)
+roof.position.y = 2.5 + 0.5
+roof.rotation.y = Math.PI * 0.25
+house.add(roof)
+
+// Door
+const door = new THREE.Mesh(
+    new THREE.PlaneGeometry(2.2, 2.2),
+    new THREE.MeshStandardMaterial({ roughness: 0.7 })
+)
+door.position.z = 2 + 0.01
+door.position.y = 1
+house.add(door)
+
+// Bush
+const bushGeometry = new THREE.SphereGeometry(1, 16, 16)
+const bushMaterial = new THREE.MeshStandardMaterial({ roughness: 0.7 })
+
+const bush1 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush1.scale.set(0.5, 0.5, 0.5)
+bush1.position.set(0.8, 0.2, 2.2)
+house.add(bush1)
+
+const bush2 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush2.scale.set(0.25, 0.25, 0.25)
+bush2.position.set(1.4, 0.1, 2.1)
+house.add(bush2)
+
+const bush3 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush3.scale.set(0.4, 0.4, 0.4)
+bush3.position.set(-0.8, 0.1, 2.2)
+house.add(bush3)
+
+const bush4 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush4.scale.set(0.15, 0.15, 0.15)
+bush4.position.set(-1, 0.05, 2.6)
+house.add(bush4)
+
+// Graves
+
+const graveGeometry = new THREE.BoxGeometry(0.6, 0.8, 0.2)
+const graveMaterial = new THREE.MeshStandardMaterial({ roughness: 0.7 })
+
+const graves = new THREE.Group()
+scene.add(graves)
+
+for (let i = 0; i < 30; i++)
+{
+    const angle = Math.random() * Math.PI * 2
+    const radius = 3 + Math.random() * 4
+    const x = Math.sin(angle) * radius
+    const z = Math.cos(angle) * radius
+
+    const grave = new THREE.Mesh(graveGeometry, graveMaterial)
+    grave.position.x =  x
+    grave.position.z =  z
+    grave.position.y = Math.random() * 0.4
+    grave.rotation.x = Math.random() - 0.5
+    grave.rotation.y = Math.random() - 0.5
+    grave.rotation.z = Math.random() - 0.5
+
+    graves.add(grave)
+}
+
+// Texturing
+
+
 
 /**
  * Lights
