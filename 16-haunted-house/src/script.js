2c28400e5f60c3a6f816999467947a58adbece6f
oscar1223 <oscararauzp@gmail.com>
2025-02-04 18:44:43 +0100

    textures added

--- 16-haunted-house/src/script.js
+++ 16-haunted-house/src/script.js
@@ -43,11 +43,60 @@ floorNormalTexture.wrapT = THREE.RepeatWrapping
 floorDisplacementTexture.wrapS = THREE.RepeatWrapping
 floorDisplacementTexture.wrapT = THREE.RepeatWrapping
 
+// Walls
+const wallColorTexture = textureLoader.load('./16-haunted-house-resources/wall/castle_brick_broken_06_1k/castle_brick_broken_06_diff_1k.jpg')
+const wallARMTexture = textureLoader.load('./16-haunted-house-resources/wall/castle_brick_broken_06_1k/castle_brick_broken_06_arm_1k.jpg')        
+const wallNormalTexture = textureLoader.load('./16-haunted-house-resources/wall/castle_brick_broken_06_1k/castle_brick_broken_06_nor_1k.jpg')          
+
+wallColorTexture.colorSpace = THREE.SRGBColorSpace
+
+// Bush 
+const bushColorTexture = textureLoader.load('./16-haunted-house-resources/bush/leaves_forest_ground_1k/leaves_forest_ground_diff_1k.jpg')
+const bushARMTexture = textureLoader.load('./16-haunted-house-resources/bush/leaves_forest_ground_1k/leaves_forest_ground_arm_1k.jpg')        
+const bushNormalTexture = textureLoader.load('./16-haunted-house-resources/bush/leaves_forest_ground_1k/leaves_forest_ground_nor_gl_1k.jpg')          
+
+bushColorTexture.colorSpace = THREE.SRGBColorSpace
+
+
+// Graves
+const graveColorTexture = textureLoader.load('./16-haunted-house-resources/grave/plastered_stone_wall_1k/plastered_stone_wall_diff_1k.jpg')
+const graveARMTexture = textureLoader.load('./16-haunted-house-resources/grave/plastered_stone_wall_1k/plastered_stone_wall_arm_1k.jpg')        
+const graveNormalTexture = textureLoader.load('./16-haunted-house-resources/grave/plastered_stone_wall_1k/plastered_stone_wall_nor_1k.jpg')          
+
+graveColorTexture.colorSpace = THREE.SRGBColorSpace
+
+// Door
+const doorColorTexture = textureLoader.load('./door/color.jpg')
+const doorAlphaTexture = textureLoader.load('./door/alpha.jpg')
+const doorAmbientOcclusionTexture = textureLoader.load('./door/ambientOcclusion.jpg')
+const doorHeightTexture = textureLoader.load('./door/height.jpg')
+const doorNormalTexture = textureLoader.load('./door/normal.jpg')
+const doorMetalnessTexture = textureLoader.load('./door/metalness.jpg')
+const doorRoughnessTexture = textureLoader.load('./door/roughness.jpg')
+
+doorColorTexture.colorSpace = THREE.SRGBColorSpace
+
+
+// Roof
+const roofColorTexture = textureLoader.load('./16-haunted-house-resources/roof/roof_slates_02_1k/roof_slates_02_diff_1k.jpg') 
+const roofARMTexture = textureLoader.load('./16-haunted-house-resources/roof/roof_slates_02_1k/roof_slates_02_arm_1k.jpg')
+const roofNormalTexture = textureLoader.load('./16-haunted-house-resources/roof/roof_slates_02_1k/roof_slates_02_nor_1k.jpg')
+
+roofColorTexture.colorSpace = THREE.SRGBColorSpace
+
+roofColorTexture.repeat.set(3, 1)
+roofARMTexture.repeat.set(3, 1)
+roofNormalTexture.repeat.set(3, 1)
+
+roofColorTexture.wrapS = THREE.RepeatWrapping
+roofARMTexture.wrapS = THREE.RepeatWrapping
+roofNormalTexture.wrapS = THREE.RepeatWrapping
+
+
 
 /**
  * House
  */
-
 
 // Floor
 const floor = new THREE.Mesh(
@@ -75,7 +124,13 @@ scene.add(house)
 // walls
 const walls = new THREE.Mesh(
     new THREE.BoxGeometry(4, 2.5, 4),
-    new THREE.MeshStandardMaterial({ roughness: 0.7 })
+    new THREE.MeshStandardMaterial({ 
+        map: wallColorTexture,
+        aoMap: wallARMTexture,
+        roughnessMap: wallARMTexture,
+        metalnessMap: wallARMTexture,
+        normalMap: wallNormalTexture,
+     })
 )
 walls.position.y = 1.25
 
@@ -84,7 +139,13 @@ house.add(walls)
 // Roof
 const roof = new THREE.Mesh(
     new THREE.ConeGeometry(3.5, 1, 4),
-    new THREE.MeshStandardMaterial({ roughness: 0.7 })
+    new THREE.MeshStandardMaterial({
+        map: roofColorTexture,
+        aoMap: roofARMTexture,
+        roughnessMap: roofARMTexture,
+        metalnessMap: roofARMTexture,
+        normalMap: roofNormalTexture,
+    })
 )
 roof.position.y = 2.5 + 0.5
 roof.rotation.y = Math.PI * 0.25
@@ -92,8 +153,18 @@ house.add(roof)
 
 // Door
 const door = new THREE.Mesh(
-    new THREE.PlaneGeometry(2.2, 2.2),
-    new THREE.MeshStandardMaterial({ roughness: 0.7 })
+    new THREE.PlaneGeometry(2.2, 2.2, 5, 5),
+    new THREE.MeshStandardMaterial({ 
+        map: doorColorTexture,
+        transparent: true,
+        alphaMap: doorAlphaTexture,
+        aoMap: doorAmbientOcclusionTexture,
+        displacementMap: doorHeightTexture,
+        displacementScale: 0.15,
+        normalMap: doorNormalTexture,
+        metalnessMap: doorMetalnessTexture,
+        roughnessMap: doorRoughnessTexture,
+     })
 )
 door.position.z = 2 + 0.01
 door.position.y = 1
@@ -101,32 +172,49 @@ house.add(door)
 
 // Bush
 const bushGeometry = new THREE.SphereGeometry(1, 16, 16)
-const bushMaterial = new THREE.MeshStandardMaterial({ roughness: 0.7 })
+const bushMaterial = new THREE.MeshStandardMaterial({ 
+    color: '#ccffcc',
+    map: bushColorTexture,
+    aoMap: bushARMTexture,
+    roughnessMap: bushARMTexture,
+    metalnessMap: bushARMTexture,
+    normalMap: bushNormalTexture,
+ })
 
 const bush1 = new THREE.Mesh(bushGeometry, bushMaterial)
 bush1.scale.set(0.5, 0.5, 0.5)
 bush1.position.set(0.8, 0.2, 2.2)
+bush1.rotation.x = -0.75
 house.add(bush1)
 
 const bush2 = new THREE.Mesh(bushGeometry, bushMaterial)
 bush2.scale.set(0.25, 0.25, 0.25)
 bush2.position.set(1.4, 0.1, 2.1)
+bush2.rotation.x = -0.75
 house.add(bush2)
 
 const bush3 = new THREE.Mesh(bushGeometry, bushMaterial)
 bush3.scale.set(0.4, 0.4, 0.4)
 bush3.position.set(-0.8, 0.1, 2.2)
+bush3.rotation.x = -0.75
 house.add(bush3)
 
 const bush4 = new THREE.Mesh(bushGeometry, bushMaterial)
 bush4.scale.set(0.15, 0.15, 0.15)
 bush4.position.set(-1, 0.05, 2.6)
+bush4.rotation.x = -0.75
 house.add(bush4)
 
 // Graves
 
 const graveGeometry = new THREE.BoxGeometry(0.6, 0.8, 0.2)
-const graveMaterial = new THREE.MeshStandardMaterial({ roughness: 0.7 })
+const graveMaterial = new THREE.MeshStandardMaterial({ 
+    map: graveColorTexture,
+    aoMap: graveARMTexture,
+    roughnessMap: graveARMTexture,
+    metalnessMap: graveARMTexture,
+    normalMap: graveNormalTexture,
+ })
 
 const graves = new THREE.Group()
 scene.add(graves)
